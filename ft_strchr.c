89bd0ceaec4fd74c575e723fe97182890b85708f
ramzerk <ramzerk@MacBook-Air-de-Chayma.local>
2023-11-02 11:13:56 +0100

    c

---
+++ ft_strchr.c
@@ -0,0 +1,12 @@
+#include "libft.h"
+
+char *strchr(const char *str, int c)
+{
+    char *result;
+    int i;
+
+    i = 0;
+    while (str[i]!= c)
+        i++;
+    
+}
---
+++ libft.h
@@ -0,0 +1,10 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char *strchr(const char *str, int c)
+
+
+#endif
