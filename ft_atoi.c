2374138ece27d0dcc150d5a68f7d043672468354
Ramzy Bouziane <rabouzia@bess-f1r2s3.clusters.42paris.fr>
2023-11-07 18:48:56 +0100

    07/11/23 part 1,5

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,33 +6,34 @@
 /*   By: rabouzia <rabouzia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 14:28:08 by rabouzia          #+#    #+#             */
-/*   Updated: 2023/11/06 14:28:09 by rabouzia         ###   ########.fr       */
+/*   Updated: 2023/11/07 18:48:20 by rabouzia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
- int ft_atoi(char *str)
+int	ft_atoi(char *str)
 {
-    int p;
-    int n;
-    n = 0;
-    p = 0;
-    while ((*str >= 9 || *str <= 13) && *str == 32)
-        str++;
-    while (*str == '-' || *str == '+')
-    {   
-        if (*str == '-')
-            p++;
-        str++;
-    }
-    while (*str >= '0' && *str <= '9')
-    {
-        n *= 10;
-        n += *str - '0';
-        str++;         
-    }
-    if (!(p % 2 == 0))
-        return -n;
-    return n;
+	int	p;
+	int	n;
+
+	n = 0;
+	p = 0;
+	while ((*str >= 9 || *str <= 13) && *str == 32)
+		str++;
+	while (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			p++;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		n *= 10;
+		n += *str - '0';
+		str++;
+	}
+	if (!(p % 2 == 0))
+		return (-n);
+	return (n);
 }
