31a224aa10c047a442c7e244562069540c794708
Ramzy Bouziane <rabouzia@paul-f4Ar7s6.clusters.42paris.fr>
2023-11-09 15:58:56 +0100

    asdasd

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,32 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rabouzia <rabouzia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/08 14:45:20 by rabouzia          #+#    #+#             */
-/*   Updated: 2023/11/09 15:47:19 by rabouzia         ###   ########.fr       */
+/*   Created: 2023/11/09 15:35:47 by rabouzia          #+#    #+#             */
+/*   Updated: 2023/11/09 15:47:27 by rabouzia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
 	size_t	i;
-	size_t	x;
+	size_t	j;
+	size_t	dlen;
+	size_t	slen;
 
-	x = ft_strlen(src);
 	i = 0;
-	if (size != 0)
+	j = 0;
+	while (dest[j] != '\0')
 	{
-		while (src [i] != '\0' && i < size - 1)
-		{
-			dest[i] = src[i];
-			i++;
-		}
-		dest[i] = '\0';
+		j++;
 	}
-	return (x);
+	dlen = j;
+	slen = ft_strlen(src);
+	if (size == 0 || size <= dlen)
+		return (slen + size);
+	while (src [i] != '\0' && i < size - dlen - 1)
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
+	return (dlen + slen);
 }
